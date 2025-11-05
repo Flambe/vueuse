daa8897861eb7e06af9693575528c254ecc69455
Steve Axtmann <steve.axtmann@runway36.de>
2025-11-05 16:51:58 +0100

    chore: release v14.0.5

--- packages/shared/whenever/index.ts
+++ packages/shared/whenever/index.ts
@@ -1,4 +1,5 @@
 import type { WatchCallback, WatchHandle, WatchOptions, WatchSource } from 'vue'
+import type { MapOldSources, MapSources } from '../utils'
 import { nextTick, watch } from 'vue'
 
 export interface WheneverOptions extends WatchOptions {
@@ -12,13 +13,13 @@ export interface WheneverOptions extends WatchOptions {
   once?: boolean
 }
 
-type MapSources<T> = {
-  [K in keyof T]: T[K] extends WatchSource<infer V> ? V : T[K] extends object ? T[K] : never;
-}
+// type MapSources<T> = {
+//   [K in keyof T]: T[K] extends WatchSource<infer V> ? V : T[K] extends object ? T[K] : never;
+// }
 
 // overloads
+export function whenever<T extends Readonly<WatchSource<unknown>[]>>(sources: readonly [...T] | T, cb: WatchCallback<MapSources<T>, MapOldSources<T, true>>, options?: WheneverOptions): WatchHandle
 export function whenever<T>(source: WatchSource<T | false | null | undefined>, cb: WatchCallback<T>, options?: WheneverOptions): WatchHandle
-export function whenever<T extends Readonly<WatchSource<unknown>[]>>(sources: readonly [...T] | T, cb: WatchCallback<MapSources<T>, MapSources<T>>, options?: WheneverOptions): WatchHandle
 export function whenever<T extends object>(source: T, cb: WatchCallback<T>, options?: WheneverOptions): WatchHandle
 
 /**
